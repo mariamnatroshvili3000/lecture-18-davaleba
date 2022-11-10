b84118e8ac388de9b9767cb9daff48756d7c0c7f
mariamnatroshvili3000 <112975366+mariamnatroshvili3000@users.noreply.github.com>
2022-11-10 21:53:52 +0400

    hgjm
    hfjhg

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function(){
+    count-=1;
+}
+document.getElementById("resetBtn").onclick = function(){
+    count-=0
+}
+document.getElementById("increaseBtn").onclick = function(){
+     count-=1;
+}
